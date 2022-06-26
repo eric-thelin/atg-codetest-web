b107a46f87dc7578de21d96f41acb368ef44c5c6
Eric Thelin <me@ericthelin.se>
2022-06-26 12:19:08 +0200

    Create simple Selenium test for opening the ATG webite
    Note: In the test assumes that there is local Selenium server running
    providing a Chrome browser. One way to acheive this is by using the
    Docker Compose configuration in the project root, i.e.
    
    	docker-compose up -d
    
    See: https://github.com/SeleniumHQ/docker-selenium

--- src/test/java/com/zingtongroup/atg/codetest/web/SeleniumWebTest.java
+++ src/test/java/com/zingtongroup/atg/codetest/web/SeleniumWebTest.java
@@ -1,13 +1,36 @@
 package com.zingtongroup.atg.codetest.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumWebTest {
 
+	private WebDriver driver;
+
+	@BeforeEach
+	void createDriver() {
+		driver = new RemoteWebDriver(new ChromeOptions());
+	}
+
+	@AfterEach
+	void quitDriver() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
 	@Test
-	void dummy() {
-		assertTrue(true);
+	void visitSite() {
+		// When
+		driver.get("https://www.atg.se/");
+
+		// Then
+		assertEquals("ATG - Spel på Sport, Häst och Casino", driver.getTitle());
 	}
 }
