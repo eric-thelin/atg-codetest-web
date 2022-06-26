72d2ebeca8eabc5bd3917c3bf0571269715f31ec
Eric Thelin <me@ericthelin.se>
2022-06-26 12:19:08 +0200

    Create dummy test

---
+++ src/test/java/com/zingtongroup/atg/codetest/web/SeleniumWebTest.java
@@ -0,0 +1,13 @@
+package com.zingtongroup.atg.codetest.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SeleniumWebTest {
+
+	@Test
+	void dummy() {
+		assertTrue(true);
+	}
+}
