e94987e8caa645a3bf9dfae637ea2b705ad4a2ad
Eric Thelin <me@ericthelin.se>
2022-07-01 10:54:36 +0200

    Test that we can write to files in missing directories

--- src/test/java/se/ericthelin/atg/codetest/web/support/RealFileSystemTest.java
+++ src/test/java/se/ericthelin/atg/codetest/web/support/RealFileSystemTest.java
@@ -2,13 +2,17 @@ package se.ericthelin.atg.codetest.web.support;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RealFileSystemTest {
@@ -17,10 +21,14 @@ class RealFileSystemTest {
 	private File root;
 	private final RealFileSystem subject = new RealFileSystem();
 
-	@Test
-	void writesBytesToDestination() throws IOException {
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"out.txt",
+			"a/b/c/d/bar.png"
+	})
+	void writesBytesToDestination(String fileName) throws IOException {
 		// Given
-		File destination = new File(root, "out.txt");
+		File destination = new File(root, fileName);
 
 		// When
 		subject.write("foo".getBytes(StandardCharsets.UTF_8), destination);
@@ -28,6 +36,26 @@ class RealFileSystemTest {
 		// Then
 		assertTrue(destination.exists());
 		assertEquals("foo", contentsOf(destination));
+	}
+
+	@Test
+	void abortsWhenUnableToCreateParentDirectories() throws IOException {
+		// Given
+		File readOnlyDirectory = new File(root, "a/b/c");
+		assertTrue(readOnlyDirectory.mkdirs());
+		assertTrue(readOnlyDirectory.setWritable(false));
+		File destination = new File(readOnlyDirectory, "d/out.txt");
+
+		// When
+		String message = assertThrows(IOException.class, () ->
+				subject.write("foo".getBytes(StandardCharsets.UTF_8), destination)
+		).getMessage();
+
+		// Then
+		assertAll("Unexpected message: " + message,
+				() -> assertTrue(message.startsWith("Could not create directory")),
+				() -> assertTrue(message.contains(readOnlyDirectory.getAbsolutePath()))
+		);
 	}
 
 	private String contentsOf(File source) throws IOException {
